88601aaa69dff406b7c0d9f376bc9e68b9506a64
Alperen <alperen@Alperens-MacBook-Pro.local>
2024-04-12 15:20:33 +0800

    added new feature model

--- automation/reproduce_results.py
+++ automation/reproduce_results.py
@@ -8,7 +8,11 @@ def run_whole_experiment(template_file : str = "template.txt"):
     subprocess.run(f"python main.py -t {template_file}", shell=True)
     
 def main():
-    sampling_models = [os.path.join("isolated_feature_templates", "Improved_soccer_model_f2_gridExpansion.txt.txt"), os.path.join("isolated_feature_templates", "Improved_soccer_model_f2_gridExpansion.txt")]
+    sampling_models = [
+        os.path.join("isolated_feature_templates", "Improved_soccer_model_f2_gridExpansion.txt.txt"), 
+        os.path.join("isolated_feature_templates", "Improved_soccer_model_f2_gridExpansion.txt"), 
+        os.path.join("isolated_feature_templates", "Improved_soccer_model_f1_penalty.txt")
+        ]
     
     print("running experiments for isolated features")
     for model in sampling_models:
