487b2ee14a93b87455acadd9891257f059f977c6
bharathraj25 <bharathrajgngowdara@gmail.com>
2024-04-13 23:46:48 +0800

    chore: push data

---
+++ automation-dynamic-v1/config.py
@@ -0,0 +1,11 @@
+import os 
+
+PAT_CONSOLE_EXE_DIRECTORY = "/Users/alperen/Downloads/PAT_351/PAT3.Console-2.exe"
+
+BETTING_SIMULATION_DIR = os.path.join(".", "betting_simulation")
+BETTING_DATASET_DIR = os.path.join(".", "betting_simulation/betting_dataset")
+MATCHES_DATASET_DIR = os.path.join(".", "Datasets/matches")
+RATINGS_DATASET_DIR = os.path.join(".", "Datasets/ratings")
+TEMPLATE_FILE = os.path.join(".", "template.txt")
+SAMPLE_SIZE = 10
+RANDOM_SEED = 42
